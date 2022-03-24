c5256fd8307f4f84d675abecbec6a7aa21b16ff5
CKURZ <ckurz098@gmail.com>
2022-03-24 10:23:16 -0700

    Finished contact form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from "./components/Gallery";
 import ContactForm from "./components/Contact";
 
 function App() {
+
   const [categories] = useState([
     {
       name: 'commercial',
@@ -15,25 +16,30 @@ function App() {
     { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
   ]);
 
+
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [contactSelected, setContactSelected] = useState(false);
 
   return (
     <div>
       <Nav
-      categories = {categories}
-      setCurrentCategory = {setCurrentCategory}
-      currentCategory = { currentCategory}
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        
-        <ContactForm></ContactForm>
 
-        <Gallery
-         currentCategory = { currentCategory}
-        >
-       
-        </Gallery>
-        <About></About>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory} />
+            <About />
+          </>
+        ) : (
+          <ContactForm />
+        )}
+
       </main>
     </div>
   );
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,17 +1,39 @@
 import React, { useState } from "react";
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
     const { name, email, message } = formState;
 
+    const [errorMessage, setErrorMessage] = useState('');
+
     function handleChange(e) {
-        //update for state with everything from formstate and also update name
-        setFormState({ ...formState, [e.target.name]: e.target.value })
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+
+        if (!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: e.target.value });
+        }
     }
+    console.log(formState);
 
     function handleSubmit(e) {
         e.preventDefault();
-        console.log(formState);
+        if (name && email && message) {
+            console.log('Submit!! ', formState);
+        }
     }
 
     return (
@@ -30,6 +52,12 @@ function ContactForm() {
                     <label htmlFor="message">Message:</label>
                     <textarea name="message" rows="5" defaultValue={message} onChange={handleChange} />
                 </div>
+
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
 
                 <button type="submit">Submit</button>
             </form>
